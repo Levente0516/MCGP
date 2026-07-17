3370095eef7b96f907901913f8ad8d11ec2b4476
Levente0516 <levente0517@gmail.com>
2026-07-17 15:42:52 +0200

    solution (kinda)

--- src/CurvSolution/test.m
+++ src/CurvSolution/test.m
@@ -6,20 +6,34 @@ curve.y = @(t) cos(t);
 curve.alpha = 0;    %lower boundary for t
 curve.beta = 2*pi;  %upper boundary for t   t \in [alpha, beta]
 
-curve.div = 100; %divison of linspace
+curve.div.def = 1000; %divison of linspace
+curve.div.x = 200;
+curve.div.y = 200;
 
 %Default
 curve.boundary(1).alpha = 0;
-curve.boundary(1).beta = 1.2*pi;
+curve.boundary(1).beta = 0.7*pi;
 curve.boundary(1).type = "D";
-curve.boundary(1).value = 0;
+curve.boundary(1).value = @(x,y) sin(3*x).*cos(2*y);
 
-curve.boundary(2).alpha = 1.2*pi;
-curve.boundary(2).beta = 2*pi;
-curve.boundary(2).type = "N";
-curve.boundary(2).value = 0;
+curve.boundary(2).alpha = 0.7*pi;
+curve.boundary(2).beta = 1.2*pi;
+curve.boundary(2).type = "D";
+curve.boundary(2).value = @(x,y) sin(3*x).*cos(2*y);
 
-curve.bounds = 2;
+curve.boundary(3).alpha = 1.2*pi;
+curve.boundary(3).beta = 1.8*pi;
+curve.boundary(3).type = "D";
+curve.boundary(3).value = @(x,y) sin(3*x).*cos(2*y);
 
-%Solver(curve)
-visualize(curve)
+curve.boundary(4).alpha = 1.8*pi;
+curve.boundary(4).beta = 2*pi;
+curve.boundary(4).type = "D";
+curve.boundary(4).value = @(x,y) sin(3*x).*cos(2*y);
+
+curve.bounds = 4;
+
+curve.iterSteps = 1000;
+ 
+u = Solver(curve);
+visualize(curve, u)
--- src/CurvSolution/visualize.m
+++ src/CurvSolution/visualize.m
@@ -1,33 +1,61 @@
-function [] = visualize(curve)
+function [] = visualize(curve, u)
 
-t = linspace(curve.alpha,curve.beta,curve.div);
+t = linspace(curve.alpha,curve.beta,curve.div.def);
 
 x = curve.x(t);
 y = curve.y(t);
 
-hold on;
-plot(x,y,'Color','w')
-axis equal tight
-grid on
+xx = linspace(min(x), max(x), curve.div.x);
+yy = linspace(min(y), max(y), curve.div.y);
 
-for n = 1:curve.bounds
+[X,Y] = meshgrid(xx,yy);
 
-    if  curve.boundary(n).type == "D"
-        tt = linspace(curve.boundary(n).alpha, ...
-            curve.boundary(n).beta, curve.div);
-        xx = curve.x(tt);
-        yy = curve.y(tt);
-        plot(xx,yy, 'Color', 'r')
+inside = inpolygon(X,Y,x,y);
 
-    elseif curve.boundary(n).type == "N"
-        tt = linspace(curve.boundary(n).alpha, ...
-            curve.boundary(n).beta, curve.div);
-        xx = curve.x(tt);
-        yy = curve.y(tt);
-        plot(xx,yy, 'Color', 'b')
+u_plot = u;
+u_plot(~inside) = NaN;
 
-    end
+%hold on;
+%plot(x,y,'Color','w')
+%axis equal tight
+%grid on
 
-end
+%for n = 1:curve.bounds
+%
+%    if  curve.boundary(n).type == "D"
+%        tt = linspace(curve.boundary(n).alpha, ...
+%            curve.boundary(n).beta, curve.div);
+%        xx = curve.x(tt);
+%        yy = curve.y(tt);
+%        plot(xx,yy, 'Color', 'r')
+%
+%    elseif curve.boundary(n).type == "N"
+%        tt = linspace(curve.boundary(n).alpha, ...
+%            curve.boundary(n).beta, curve.div);
+%        xx = curve.x(tt);
+%        yy = curve.y(tt);
+%        plot(xx,yy, 'Color', 'b')
+%
+%    end
+%
+%end
 
-hold off;
+subplot(1,2,1);
+
+contourf(X, Y, u_plot, 20, 'LineColor','none');
+colorbar;
+xlabel('x'); 
+ylabel('y');
+title('');
+axis equal tight; 
+colormap turbo;
+
+
+subplot(1,2,2)
+
+surf(X,Y,u_plot)
+xlabel("x")
+ylabel("y")
+zlabel("u(x,y)")
+title("")
+shading interp
